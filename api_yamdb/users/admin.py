29a0bd217b8c2e798d2060ec3c0986990efdc5e5
RBekr <47711432+RBekr@users.noreply.github.com>
2023-02-01 13:00:41 +0700

    добавил приложение users и класс User

---
+++ api_yamdb/users/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    list_display = ('id', 'username', 'email', 'bio', 'role')
+    search_fields = ('username',)
+    list_filter = ('id',)
+    empty_value_display = '-пусто-'
