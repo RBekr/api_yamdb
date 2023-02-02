cd2a3712cb9058459f5d76b666d30b184972b7ea
Никита Зеленин <nikita.zelenin2013@yandex.ru>
2023-02-02 14:49:10 +0500

    правки Title

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, permissions, viewsets
+from rest_framework import filters
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
 from rest_framework.pagination import LimitOffsetPagination
 from reviews.models import Title
 
 from .serializers import TitleSerializer
 
 
-class TitleViewSet(viewsets.ModelViewSet):
+class TitleViewSet(ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
-    permission_classes = 1
+    permission_classes = AllowAny
     pagination_class = LimitOffsetPagination
     filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     filterset_fields = ('category', 'genre', 'name', 'year')
     search_fields = ('genre', 'category')
-
-    def perform_create(self, serializer):
-        serializer.save()
-
+    
     def get_review(self):
         reviews = get_object_or_404(Review, pk=self.kwargs.get('title_id'))
         return reviews
